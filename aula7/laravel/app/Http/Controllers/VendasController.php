265b5e51e167465a799203a5dbcdcb72f839cdae
keziameirelles <telzinha@gmail.com>
2019-11-25 20:53:18 -0200

    aulatotal

---
+++ aula7/laravel/app/Http/Controllers/VendasController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VendasController extends Controller
+{
+    public function index()
+    {
+        $controller = 'Vendas Controller';
+
+        return view('vendas',['controller' => $controller]);
+    }
+}
---
+++ aula9/mock.php
@@ -0,0 +1,11 @@
+<?php
+
+require "vendor/autoload.php"
+
+class AlgumaClasse
+{
+public function fazAlgo()
+{
+    //funcao a implementar
+}
+}
